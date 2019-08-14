e651e10052916556f8ad390c7edb38d3369c56c4
Mohanraj <kmmraj@gmail.com>
2019-08-14 17:41:11 +0530

    Working version with Flutter Beta

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GitHub Repo Detail',
       theme: ThemeData(
-        primaryColor: Colors.blue,
-        accentColor: Colors.orange
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
       ),
       home: MyHomePage('GitHub Repo Detail'),
     );
@@ -28,32 +28,106 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body:
-        Card(
-          color: Colors.grey,
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                leading: CircleAvatar(),
-                title: Text("GH repo 1..."),
-                onTap: () {},
-                subtitle: Text("Details"),
+      body: Card(
+        //color: Colors.grey,
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 80.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(90.0)),
+                child: Image.network(
+                    'https://avatars2.githubusercontent.com/u/6253321?s=460&v=4'),
               ),
-
-
-
-              ButtonTheme.bar(
-                child: ButtonBar(children: <Widget>[
-                  RaisedButton(child: Text("Details", style: TextStyle(
-                    color: Theme.of(context).accentColor
-                  ),),onPressed: (){},),
-                  FlatButton(child: Text("Back"), onPressed: (){},)
-                ],),
-              )
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Text("Title",
+                  style: TextStyle(
+                      color: Theme.of(context).accentColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Text("Staggers"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("2"),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Text("Forks"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("2"),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Text("Language"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Kotlin"),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Text(
+                "Description..................",
+                style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text(
+                      "Details",
+                      style: TextStyle(
+                          //color: Theme.of(context).accentColor
+                          ),
+                    ),
+                    onPressed: () {},
+                  ),
+                  RaisedButton(
+                    child: Text("Back"),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )
+          ],
         ),
-
+      ),
     );
   }
 }
